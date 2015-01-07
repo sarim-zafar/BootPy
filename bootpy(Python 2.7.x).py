76fe47bf74f039bc54dfe1cecbd503ace1a94d13
Sarim Zafar <sarim.zafar@yahoo.com>
2015-01-08 02:48:50 +0500

    Updated the cdn[Again]

--- bootpy(Python 2.7.x).py
+++ bootpy(Python 2.7.x).py
@@ -18,8 +18,6 @@ def end():
     <!----------------Bootstrap core JavaScript------------------->
     <!--   ==================================================   -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="http://ironsummitmedia.github.io/startbootstrap-simple-sidebar/js/jquery.js"></script>
-    <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
